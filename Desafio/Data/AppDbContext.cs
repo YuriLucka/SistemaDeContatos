fd47822a1c25f03fb45fc49b865b54923f600c8f
Yuri <Yuri@DESKTOP-CBSOHAM>
2025-03-07 09:32:40 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Desafio.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio
+{
+	public class AppDbContext : DbContext
+	{
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Contato> Contato { get; set; }
+	}
+}
---
+++ Desafio/Models/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio.Models
+{
+	public class Contato
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; } = string.Empty;
+		public string Telefone { get; set; } = string.Empty;
+		public string Cidade { get; set; } = string.Empty;
+	}
+}
